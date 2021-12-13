f803a804ecbbd43e3cc183ab835bcccf965c5342
txvvgnx <txvvgnx@gmail.com>
2021-12-13 23:22:02 +0800

    Fixed motorpair library

--- lib/motorpair/ev3wrapmotorpair.cpp
+++ lib/motorpair/ev3wrapmotorpair.cpp
@@ -1,25 +1,21 @@
 #include <ev3wrapmotorpair.hpp>
 
-Ev3Wrap::MotorPair::MotorPair(Ev3Wrap::Motor motor1, Ev3Wrap::Motor motor2)
-{
-    this->motor1 = motor1;
-    this->motor2 = motor2;
+Ev3Wrap::MotorPair::MotorPair(Ev3Wrap::Motor m1, Ev3Wrap::Motor m2) {
+    this->motor1 = m1;
+    this->motor2 = m2;
 }
 
-Ev3Wrap::Motor move(float rpm)
-{
-    motor1.runForever(rpm);
-    motor2.runForever(rpm);
+void move(float rpm) {
+    Ev3Wrap::MotorPair::motor1.runForever(rpm);
+    Ev3Wrap::MotorPair::motor2.runForever(rpm);
 }
 
-Ev3Wrap::Motor stop()
-{
-    motor1.stop();
-    motor2.stop();
+void stop() {
+    Ev3Wrap::MotorPair::motor1.holdPosition();
+    Ev3Wrap::MotorPair::motor2.holdPosition();
 }
 
-Ev3Wrap::Motor Ev3Wrap::CreateMotorPair(Ev3Wrap::Motor motor1, Ev3Wrap::Motor motor2)
-{
-    Ev3Wrap::CreateMotorPair retMotorPair = Ev3Wrap::MotorPair(motor1, motor2);
+Ev3Wrap::MotorPair Ev3Wrap::MotorPair::CreateMotorPair(Ev3Wrap::Motor m1, Ev3Wrap::Motor m2) {
+    Ev3Wrap::MotorPair retMotorPair = Ev3Wrap::MotorPair(m1, m2);
     return retMotorPair;
 }
