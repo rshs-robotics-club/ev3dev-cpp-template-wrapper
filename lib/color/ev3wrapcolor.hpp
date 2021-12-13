1f78ce6830cd7c02ed0a66adc8df62748b93835e
txvvgnx <txvvgnx@gmail.com>
2021-12-13 18:24:03 +0800

    Attempted to use enum class

--- lib/color/ev3wrapcolor.hpp
+++ lib/color/ev3wrapcolor.hpp
@@ -4,6 +4,17 @@
 
 namespace Ev3Wrap {
 
+enum class Color {
+    NONE,
+    BLACK,
+    BLUE,
+    GREEN,
+    YELLOW,
+    RED,
+    WHITE,
+    BROWN
+};
+
 class ColorSensor : private ev3dev::color_sensor {
     public:
         static ColorSensor bind(ev3dev::address_type addr = ev3dev::INPUT_AUTO);
@@ -13,17 +24,10 @@ class ColorSensor : private ev3dev::color_sensor {
         int getAmbientLightIntensity() {
             return this->ambient_light_intensity();
         }
-        int getColor() {
-             // Color detected by the sensor, categorized by overall value.
-            //   - 0: No color
-            //   - 1: Black
-            //   - 2: Blue
-            //   - 3: Green
-            //   - 4: Yellow
-            //   - 5: Red
-            //   - 6: White
-            //   - 7: Brown
-            return this->color();
+        Color getColor() {
+            int colorIndex = this->color();
+            Color color = static_cast<Color>(colorIndex);
+            return color;
         }
         std::tuple<int, int, int> getRgbColor() {
             return this->raw();
