dadb3a3b6689bd5cbc6725c34ef3cfeeb58c0ee9
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-12-13 15:12:29 +0800

    added 4 functions for near raw inputs

--- lib/irseeker/ev3wrapirseeker.hpp
+++ lib/irseeker/ev3wrapirseeker.hpp
@@ -7,6 +7,11 @@ namespace Ev3Wrap {
 class IrSeeker : private ev3dev::irseeker_sensor {
     public:
         static IrSeeker bind(ev3dev::address_type = ev3dev::INPUT_AUTO);
+        float getStrengthAC() {return this->ac();}
+        float getStrengthDC() {return this->dc();}
+        std::tuple<int, int, int, int, int, int> getAllAC(){return this->ac_all();}
+        std::tuple<int, int, int, int, int, int, int> getAllDC(){return this->dc_all();}
+        
     private:
         IrSeeker(ev3dev::address_type = ev3dev::INPUT_AUTO);
 };
