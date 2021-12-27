aaf5560421b0ac6d62adb7ce02cc8d4d98bd1d96
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-12-27 18:50:34 +0800

    added relative functions for compass sensor

--- lib/compass/ev3wrapcompass.hpp
+++ lib/compass/ev3wrapcompass.hpp
@@ -5,10 +5,26 @@
 namespace Ev3Wrap {
 
 class CompassSensor : private ev3dev::compass {
+    private:
+        int degreeOffset;
     public:
         static CompassSensor bind(ev3dev::address_type addr = ev3dev::INPUT_AUTO);
-        int getHeadingDegrees() {
+        // gets the absolute direction. getRelativeDirection is recommended
+        int getAbsoluteDirection() {
             return this->degrees();
+        }
+        // sets the current direction as 0 degrees (in getRelativeDirection)
+        CompassSensor& setZero() {
+            this->degreeOffset = this->getAbsoluteDirection();
+        }
+        int getDegreeOffset() {
+            return this->degreeOffset;
+        }
+        int getRelativeDirection() {
+            float ret = this->getAbsoluteDirection() - this->degreeOffset;
+            if(ret >= 360) {return ret - 360;}
+            if(ret < 0) {return ret + 360;}
+            return ret;
         }
         void beginCalibrate() {
             return this->begin_calibration();
