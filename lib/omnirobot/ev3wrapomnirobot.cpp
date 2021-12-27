da22b290b99afc3b64bd235de062d437defa0e32
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-12-27 17:58:59 +0800

    finished 2 functions

--- lib/omnirobot/ev3wrapomnirobot.cpp
+++ lib/omnirobot/ev3wrapomnirobot.cpp
@@ -1,17 +1,62 @@
 #include <ev3wrapomnirobot.hpp>
+#include <cmath>
+#include <iostream>
+using namespace Ev3Wrap;
+
 // constructor
-Ev3Wrap::Omni::Omni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass) : 
+Omni::Omni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass, Angle motorToCompassOffset) : 
     leftRightPair(leftRightPair), frontBackPair(frontBackPair), compass(compass) {
-        std::cout << "constructing Omni\n";
+    std::cout << "constructing Omni\n";
+    this->motorToCompassOffset = motorToCompassOffset;
 };
 
 // static public constructor
-Ev3Wrap::Omni Ev3Wrap::Omni::CreateOmni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass) {
-    Ev3Wrap::Omni retRobot = Ev3Wrap::Omni(leftRightPair, frontBackPair, compass);
+Omni Omni::CreateOmni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass, Angle motorToCompassOffset) {
+    Omni retRobot = Omni(leftRightPair, frontBackPair, compass, motorToCompassOffset);
     return retRobot;
 };
 
 // declare static variables
-std::map<ev3dev::address_type, int> Ev3Wrap::Motor::motorLocked = {};
+std::map<ev3dev::address_type, int> Motor::motorLocked = {};
 
-int Ev3Wrap::Motor::motorLockIncrement = 1;
+int Motor::motorLockIncrement = 1;
+
+
+std::pair<float, float> Omni::calculateMults(float x, float y, float rpm, float degrees) {
+    const float fpx = rpm;
+    const float fpy = -rpm;
+    const float rpx = rpm;
+    const float rpy = rpm;
+    const float pi = atan(1) * 4;
+    float newX = x * cos(degrees * pi / 180) - y * sin(degrees * pi / 180);
+    float newY = x * sin(degrees * pi / 180) + y * cos(degrees * pi / 180);
+    float mag = sqrt(newX * newX + newY * newY);
+    newX /= mag;
+    newY /= mag;
+    newX *= rpm;
+    newY *= rpm;
+    float forwardMult = (rpm * (rpx * newY - rpy * newX)) / (rpx * fpy - fpx * rpy);
+    float rightMult = (rpm * newY - fpy * forwardMult) / rpy;
+    return std::make_pair(forwardMult, rightMult);
+    // TODO ----------------------------------------------------------------------------------
+}
+
+// define motor related functions
+
+Omni& Omni::runRelativeToMotors(float x, float y, float rpm) {
+    const std::pair<float, float> mults = Omni::calculateMults(x, y, rpm, 0);
+    this->leftRightPair.runMotorsForever(mults.first, -mults.first);
+    this->frontBackPair.runMotorsForever(mults.second, -mults.second);
+    return *this;
+}
+
+Omni& Omni::runRelativeToCompass(float x, float y, float rpm) {
+    const std::pair<float, float> mults = Omni::calculateMults(x, y, rpm, this->motorToCompassOffset);
+    this->leftRightPair.runMotorsForever(mults.first, -mults.first);
+    this->frontBackPair.runMotorsForever(mults.second, -mults.second);
+    return *this;
+}
+
+// TODO: define moveRelativeToForwards
+
+// TODO: define all timed functions
--- lib/omnirobot/ev3wrapomnirobot.hpp
+++ lib/omnirobot/ev3wrapomnirobot.hpp
@@ -22,36 +22,40 @@ class Omni : public Blockable {
         // the compass sensor
         CompassSensor& compass;
         // private constructor of the omni class. Use Ev3Wrap::Omni::CreateOmni() instead.
-        Omni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass);
+        Omni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass, Angle motorToCompassOffset);
         // a float to store how offset the compass is from the motor (an X shaped motor configuration is 0 degrees)
         Angle motorToCompassOffset;
+        // calculate forwardMult and rightMult for the motor stuff
+        static std::pair<float, float> calculateMults(float x, float y, float rpm, float degrees);
     public:
         /*
             constructor for the omni class.
             MotorPairs need their motors in sequence with how their names are placed, (left then right) (front then back)
             a compass is required
             a X motor configuration and a compass facing "fowards" will result in a 0 for motorToCompassOffset.
+            in this case, the frontBackPair will refer to the top left and the front, and the bottom right as the back
             measure an approximate value for motorToCompassOffset
+            
         */
         static Omni CreateOmni(MotorPair& leftRightPair, MotorPair& frontBackPair, CompassSensor& compass, Angle motorToCompassOffset);
-        // fires the motors to move relative to the compass.
-        // Blocking mode nor non-blocking mode will not make any adjustments
-        Omni& runRelativeToCompass(float x, float y, float rpm = DEFAULT_MOTOR_RPM);
         // fires the motors to move relative to the motors. Completely ignores compass;
         // Blocking mode nor non-blocking mode will not make any adjustments
         Omni& runRelativeToMotors(float x, float y, float rpm = DEFAULT_MOTOR_RPM);
+        // fires the motors to move relative to the compass.
+        // Blocking mode nor non-blocking mode will not make any adjustments
+        Omni& runRelativeToCompass(float x, float y, float rpm = DEFAULT_MOTOR_RPM);
         // fires the motors to move relative to the "Forwards" direction given by the compass.
         // Blocking mode nor non-blocking mode will not make any adjustments
         Omni& runRelativeToForwards(float x, float y, float rpm = DEFAULT_MOTOR_RPM);
-        
+
         // -----------------------------------------------------------------------------------------
 
-        // run the motors to move relative to the compass.
-        // Blocking mode will cause it to make adjustments for the direction
-        Omni& runRelativeToCompassTimed(float x, float y, float milliseconds, float rpm = DEFAULT_MOTOR_RPM);
         // run the motors to move relative to the motors. Completely ignores compass;
         // Blocking mode will cause it to make adjustments for the direction
         Omni& runRelativeToMotorsTimed(float x, float y, float milliseconds, float rpm = DEFAULT_MOTOR_RPM);
+        // run the motors to move relative to the compass.
+        // Blocking mode will cause it to make adjustments for the direction
+        Omni& runRelativeToCompassTimed(float x, float y, float milliseconds, float rpm = DEFAULT_MOTOR_RPM);
         // run the motors to move relative to the "Forwards" direction given by the compass.
         // blocking mode will make the robot constantly update motor speed
         Omni& runRelativeToForwardsTimed(float x, float y, float milliseconds, float rpm = DEFAULT_MOTOR_RPM);
