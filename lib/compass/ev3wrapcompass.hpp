40173eff698014cec2f396558dfb2a43b52b8f2a
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-12-28 15:02:49 +0800

    added a compareAngles method

--- lib/compass/ev3wrapcompass.hpp
+++ lib/compass/ev3wrapcompass.hpp
@@ -1,4 +1,6 @@
 #include <ev3dev.h>
+#include <iostream>
+#include <exception>
 #ifndef EV3WRAPCOMPASS_HPP_
 #define EV3WRAPCOMPASS_HPP_
 
@@ -12,6 +14,27 @@ class CompassSensor : private ev3dev::compass {
         // gets the absolute direction. getRelativeDirection is recommended
         int getAbsoluteDirection() {
             return this->degrees();
+        }
+        // from stackoverflow.com
+        // returns 1 if otherAngle is to the right of sourceAngle,
+        //         0 if the angles are identical
+        //         -1 if otherAngle is to the left of sourceAngle
+        static int compareAngles(float sourceAngle, float otherAngle)
+        {
+            // sourceAngle and otherAngle should be in the range -180 to 180
+            float difference = otherAngle - sourceAngle;
+
+            if(difference < -180.0f)
+                difference += 360.0f;
+            if(difference > 180.0f)
+                difference -= 360.0f;
+
+            if(difference > 0.0f)
+                return 1;
+            if(difference < 0.0f)
+                return -1;
+
+            return 0;
         }
         // sets the current direction as 0 degrees (in getRelativeDirection)
         CompassSensor& setZero() {
