8232c8b82522ad23d0ceb99c04e019e2fa13a15a
Eisverygoodletter <yuetlongfeng@gmail.com>
2022-01-08 21:35:33 +0800

    swapped cmake to optimize for size

--- lib/irseeker/ev3wrapirseeker.hpp
+++ lib/irseeker/ev3wrapirseeker.hpp
@@ -13,7 +13,17 @@ class IrSeeker : private ev3dev::irseeker_sensor {
         std::vector<int> getAllDC(){return this->dc_all();}
         int getACStrength() {return this->getAllAC()[this->getDirectionAC()]; }
         int getDCStrength() {return this->getAllDC()[this->getDirectionDC()]; }
-
+        // returns 1 if ball is on the right of ir sensor
+        //         0 if ball is in front of ir sensor
+        //         -1 if ball is on the left of ir sensor
+        int getBallDirection() {
+            if (this->getDirectionAC() > 5)
+                return 1;
+            if (this->getDirectionAC() == 5)
+                return 0;
+            if (this->getDirectionAC() < 5)
+                return -1;
+        }
         
     private:
         IrSeeker(ev3dev::address_type addr = ev3dev::INPUT_AUTO);
