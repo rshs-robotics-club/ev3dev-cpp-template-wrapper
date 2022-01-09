1d272119189738ebcb6f983d38c09dcc88b46b91
Yuet Shing Feng <fengyuetshing@gmail.com>
2022-01-09 14:45:18 +0800

    done

--- lib/gyro/ev3wrapgyro.hpp
+++ lib/gyro/ev3wrapgyro.hpp
@@ -7,6 +7,14 @@ namespace Ev3Wrap {
 class Gyro : private ev3dev::gyro_sensor {
     public:
         static Gyro bind(ev3dev::address_type addr = ev3dev::INPUT_AUTO);
+        using ev3dev::gyro_sensor::angle;
+        using ev3dev::gyro_sensor::rate;
+        std::pair<int, int> getRateAndAngle() {
+            set_mode(mode_gyro_g_a);
+            return std::pair<int,int>(value(0), value(1));
+        }
+        int getTiltAngle() {return this->tilt_angle();}
+        int getTiltRate() {return this->tilt_rate();}
     private:
         Gyro(ev3dev::address_type addr = ev3dev::INPUT_AUTO);
 };
