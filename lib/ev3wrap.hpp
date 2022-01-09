5b4fde6a8c9d773fcd283b73c1637516b63b7c5b
Eisverygoodletter <yuetlongfeng@gmail.com>
2022-01-09 17:52:55 +0800

    initial commit

--- lib/ev3wrap.hpp
+++ lib/ev3wrap.hpp
@@ -1,7 +1,4 @@
-/*
-    all standard c++ naming conventions are to be followed in this file
-
-*/
+// macros supplied by cmakelists.txt (change in cmakelists.txt to change what components are used)
 #ifndef EV3WRAP_H_
 #define EV3WRAP_H_
 #define DEFAULT_MOTOR_RPM 50
@@ -10,13 +7,29 @@
 #include <map>
 #include <string>
 #include <ev3wrapblockable.hpp>
+#ifdef MOTOR_USED
 #include <ev3wrapmotor.hpp>
+#endif
+#ifdef TOUCH_USED
 #include <ev3wraptouch.hpp>
+#endif
+#ifdef ULTRASONIC_USED
 #include <ev3wrapultrasonic.hpp>
+#endif
+#ifdef IRSEEKER_USED
 #include <ev3wrapirseeker.hpp>
+#endif
+#ifdef COMPASS_USED
 #include <ev3wrapcompass.hpp>
+#endif
+#ifdef COLOR_USED
 #include <ev3wrapcolor.hpp>
+#endif
+#ifdef MOTORPAIR_USED
 #include <ev3wrapmotorpair.hpp>
+#endif
+#ifdef OMNIROBOT_USED
 #include <ev3wrapomnirobot.hpp>
+#endif
 
 #endif
