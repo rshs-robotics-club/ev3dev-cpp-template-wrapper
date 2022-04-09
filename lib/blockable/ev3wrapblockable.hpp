113ed177c88764c6e4eb2144a2b5e644db90c10b
Eisverygoodletter <yuetlongfeng@gmail.com>
2022-04-09 19:36:31 +0800

    added a generalized processBlock() function

--- lib/blockable/ev3wrapblockable.hpp
+++ lib/blockable/ev3wrapblockable.hpp
@@ -23,8 +23,48 @@
 #include <chrono>
 #include <functional>
 #include <ctime>
+#include <cmath>
 
 namespace Ev3Wrap {
+
+// Block struct
+struct Block {
+    float milliseconds;
+    std::function<void()> startFunction;
+    std::function<void()> stopFunction;
+    std::function<bool()> repeatedFunction;
+    bool endState;
+    float loopSpeed;
+    Block& setMilliseconds(float milliseconds) {
+        this->milliseconds = milliseconds;
+        return *this;
+    }
+    Block& setStartFunction(std::function<void()> startFunction) {
+        this->startFunction = startFunction;
+        return *this;
+    }
+    Block& setStopFunction(std::function<void()> stopFunction) {
+        this->stopFunction = stopFunction;
+        return *this;
+    }
+    Block& setRepeatedFunction(std::function<bool()> repeatedFunction) {
+        this->repeatedFunction = repeatedFunction;
+        return *this;
+    }
+    Block& setLoopSpeed(float loopSpeed) {
+        this->loopSpeed = loopSpeed;
+        return *this;
+    }
+    Block() {
+        milliseconds = 0;
+        startFunction = nullptr;
+        stopFunction = nullptr;
+        repeatedFunction = nullptr;
+        endState = false;
+        loopSpeed = 1;
+    }
+};
+
 
 // blockable class
 template<typename blockableReturn>
@@ -32,7 +72,34 @@ class Blockable {
     private:
         bool isBlocking;
     protected:
-        // block the thread for a number of milliseconds
+        void processBlock(Block& block) {
+            if (block.startFunction) {
+                block.startFunction();
+            }
+
+            std::clock_t startTime = std::clock();
+            while (true) {
+                if (block.milliseconds > 0) {
+                    float duration = (std::clock() - startTime) / CLOCKS_PER_SEC;
+                    if (duration > block.milliseconds) {
+                        break;
+                    }
+                }
+                if (block.repeatedFunction) {
+                    bool state = block.repeatedFunction();
+                    if (state == block.endState) {
+                        break;
+                    }
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(block.loopSpeed));
+            }
+            if (block.stopFunction) {
+                block.stopFunction();
+            }
+            return;
+        }
+        // block the thread for a number of milliseconds.
+        // deprecated. Use processBlock() instead.
         void blockMilliseconds(float milliseconds, std::function<void()> cleanupFunction = []{}) {
             if(!this->isBlocking) {return;}
             // std::chrono::milliseconds can't accept float for some reason, so we have to cast it
@@ -40,6 +107,7 @@ class Blockable {
             cleanupFunction();
         }
         // block the thread until a state is reached, then call the cleanupFunction.
+        // deprecated. Use processBlock() instead.
         void blockUntilStateReached(std::function<bool()> state, float loopSpeed = 5, std::function<void()> cleanupFunction = []{}, float timeoutMilliseconds = 10000) {
             if(!this->isBlocking) {return;}
             std::clock_t startTime = std::clock();
@@ -58,6 +126,7 @@ class Blockable {
         }
         // block the thread and force it into a while loop where the `function` is called as fast as possible, and
         // when `milliseconds` pass, stop and call the `cleanupFunction`
+        // deprecated. Use processBlock() instead.
         void blockMillisecondsAndFire(std::function<void()> function, float milliseconds, std::function<void()> cleanupFunction = []{}) {
             if(!this->isBlocking) {return;}
             std::clock_t startTime = std::clock();
