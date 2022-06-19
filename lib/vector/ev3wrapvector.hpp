b4af25c0e4f4c91d459e70186758a41d74d1ff7e
Eisverygoodletter <yuetlongfeng@gmail.com>
2022-06-19 20:09:49 +0800

    copyright(left)

--- lib/vector/ev3wrapvector.hpp
+++ lib/vector/ev3wrapvector.hpp
@@ -1,3 +1,19 @@
+/*
+    A class for a representation of a 2d vector
+
+    Copyright (c) 2022 - txvvgnx, Eisverygoodletter
+
+    This file is part of The Ev3dev C++ Wrapper Library.
+    The Ev3dev C++ Wrapper Library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+    either version 3 of the License, or (at your option) any later version.
+    The Ev3dev C++ Wrapper Library is distributed in the hope that it will be useful, 
+    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License 
+    along with The Ev3dev C++ Wrapper Library. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <cmath>
