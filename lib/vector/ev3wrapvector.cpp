48ee40e842284495c1437db71216f28a94d13ff3
Eisverygoodletter <yuetlongfeng@gmail.com>
2022-06-19 20:08:06 +0800

    more improvements

---
+++ lib/vector/ev3wrapvector.cpp
@@ -0,0 +1,3 @@
+#include <ev3wrapvector.hpp>
+
+Ev3Wrap::Vector Ev3Wrap::Vector::zero = Ev3Wrap::Vector(0, 0);
--- lib/vector/ev3wrapvector.hpp
+++ lib/vector/ev3wrapvector.hpp
@@ -17,75 +17,49 @@ public:
     Vector operator- (Vector& vec) {
         return Vector(this->x - vec.x, this->y - vec.y);
     }
+    bool operator== (Vector& vec) {
+        return (this->x == vec.x && this->y == vec.y);
+    }
 
     Vector normalize() {
-        float magnitude = this->magnitude();
+        float magnitude = this->dist();
         return Vector(this->x / magnitude, this->y / magnitude);
     }
 
-    float magnitude() {
-        float total = pow(x, 2) + pow(y, 2);
-        return sqrt(total);
+    float dist(Vector& otherVect = Vector::zero) {
+
+        if (otherVect == Vector::zero) {
+            float total = pow(x, 2) + pow(y, 2);
+            return sqrt(total);
+        } else {
+            Vector res = otherVect - *this;
+            return res.dist();
+        }
     }
 
-    float dist(Vector b = Vector(0, 0)) {
-        Vector result = b - *this;
-        return result.magnitude();
-    }
-
-    float getBearing() {
+    float getBearing(Vector& otherVect = Vector::zero) {
         float principalValueRadians = atan2f(y, x);
         float principalValueDegrees = principalValueRadians * 180 / M_PI;
         float bearing = 90 - principalValueDegrees;
         if (bearing > 180) {
             bearing -= 360;
         }
-        return bearing;
-    }
-
-    // polar coordinates (0 - 360 TRUE bearing)
-    Vector createPolarVector(float bearing, float magnitude) {
-        const float pi = atan(1) * 4;
-
-        // oh no more spaghetti
-        // quadrant 1
-        if (bearing <= 90) {
-            // sin is x, cos is y
-            float bearingInRadians = bearing * pi / 180;
-            float xComp = magnitude * sin(bearingInRadians);
-            float yComp = magnitude * cos(bearingInRadians);
-            Vector newVector(xComp, yComp);
-            return newVector;
+        if (otherVect == Vector::zero) {
+            return bearing;
         }
-        // quadrant 2
-        else if (bearing <= 180) {
-            // cos is x, sin is y (negative)
-            float bearingInRadians = (bearing - 90) * pi / 180;
-            float xComp = magnitude * cos(bearingInRadians);
-            float yComp = magnitude * -sin(bearingInRadians);
-            Vector newVector(xComp, yComp);
-            return newVector;
-        }
-        // quadrant 3
-        else if (bearing <= 270) {
-            // sin is x (negative), cos is y (negative)
-            float bearingInRadians = (bearing - 180) * pi / 180;
-            float xComp = magnitude * -sin(bearingInRadians);
-            float yComp = magnitude * -cos(bearingInRadians);
-            Vector newVector(xComp, yComp);
-            return newVector;
-        }
-        // quadrant 4
-        else if (bearing <= 360) {
-            // cos is x (negative), sin is y
-            float bearingInRadians = (bearing - 270) * pi / 180;
-            float xComp = magnitude * -cos(bearingInRadians);
-            float yComp = magnitude * sin(bearingInRadians);
-            Vector newVector(xComp, yComp);
-            return newVector;
+        else {
+            // get the bearing of the other vector
+            float otherBearing = otherVect.getBearing();
+            // get the difference between the two bearings
+            float difference = otherBearing - bearing;
+            return difference;
         }
     }
-
+    static Vector createVectorFromPolar(float bearing, float distance) {
+        // the bearing is clockwise from north
+        float radangle = (90 - bearing) * (M_PI / 180);
+        return Vector(distance * cosf(radangle), distance * sinf(radangle));
+    }
     // i and j constructor
     Vector(float i, float j) : x(i), y(j) {}
 };
